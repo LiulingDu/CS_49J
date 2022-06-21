58be3054c10d743f5e7a5b0f8321e103c7f07e6b
LiulingDu <liuling.du@sjsu.edu>
2022-06-21 02:10:11 -0700

    Assignment 1 test

--- src/edu/sjsu/assignment0/Main.java
+++ src/edu/sjsu/assignment0/Main.java
@@ -2,5 +2,15 @@ package edu.sjsu.assignment0;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello CS49J!");
+
+        double squareRoot=Math.sqrt(3.5);
+        System.out.println(squareRoot);
+        double power=Math.pow(2,5);
+        System.out.println(power);
+        int Width=2;
+        System.out.println(Width);
+
     }
 }
+
+
