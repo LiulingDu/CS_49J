50a04aa06df1342e900202a89d81c76319d71815
LiulingDu <liuling.du@sjsu.edu>
2022-07-14 13:30:50 -0700

    Assignment 4 Done!

--- src/edu/sjsu/assignment3/DesComparator.java
+++ src/edu/sjsu/assignment3/DesComparator.java
@@ -6,13 +6,13 @@ public class DesComparator implements Comparator<Appointment>{
     @Override
     public int compare(Appointment o1 , Appointment o2) {
         // compare description if description are different,otherwise compare start date and then end date.
-        if(o1.getDescription().equals(o2.getDescription())){
+        if(!o1.getDescription().equals(o2.getDescription())){
             return o1.getDescription().compareTo(o2.getDescription());
         }
-        if(o1.getStartDate().equals(o2.getStartDate())){
+        if(!o1.getStartDate().equals(o2.getStartDate())){
             return o1.getStartDate().compareTo(o2.getStartDate());
         }
-        if(o1.getEndDate().equals(o2.getEndDate())){
+        if(!o1.getEndDate().equals(o2.getEndDate())){
             return o1.getEndDate().compareTo(o2.getEndDate());
         }
         return 0;
