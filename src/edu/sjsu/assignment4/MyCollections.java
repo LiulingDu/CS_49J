cfee3412acb90297628aafec11072428cffa5aa1
LiulingDu <liuling.du@sjsu.edu>
2022-07-21 12:22:26 -0700

    Assignment 4 Part 1 Done!

--- src/edu/sjsu/assignment4/MyCollections.java
+++ src/edu/sjsu/assignment4/MyCollections.java
@@ -1,14 +1,10 @@
 package edu.sjsu.assignment4;
 
+import java.awt.event.WindowFocusListener;
 import java.io.*;
 import java.util.*;
 
 public class MyCollections {
-    /**
-     * prints the files from the first to the last
-     * @param files a collection of files
-     * @return print files and *s is used to indicate the end of the file content.
-     */
     public static void printFiles(Queue<File> files) throws IOException {
         while(!files.isEmpty()){
             File current=files.poll();
@@ -21,14 +17,11 @@ public class MyCollections {
         }
     }
 
-    /**
-     * removes every nth element of a collection of employee names
-     *
-     * @param employees a collection of String
-     * @param n         remove every nth element
-     * @return print a collection of the string with remove every 3rd element
-     */
-    public static void downsize(LinkedList<String> employees, int n){
+    private static void print(File file){
+
+    }
+
+    public static void downsize(LinkedList<String> employees,int n){
         ListIterator<String> listIterator=employees.listIterator();
         int index=0;
 
@@ -39,14 +32,8 @@ public class MyCollections {
                 listIterator.remove();
             }
         }
-        System.out.println(employees);
     }
 
-    /**
-     * checks if a String has balanced parentheses
-     * @param exp String
-     * @return whether it is a balanced parentheses
-     */
     public static boolean isBalanced(String exp){
         Stack<Character> parentheses= new Stack<>();
         for(char c:exp.toCharArray()){
@@ -72,6 +59,7 @@ public class MyCollections {
     public static void main(String[] args) {
         String[] list={"Tom", "Bob", "Cathy", "Alice", "Trudy", "Harry", "Denny"};
         LinkedList<String> employees=new LinkedList<>(Arrays.asList(list));
+        System.out.println("before: " + employees);
         downsize(employees,3);
         System.out.println("after: " + employees);
     }
